47d8b4eae48fc882185bc467e7745375db338f7f
Angel <113852397+AngelOncoy@users.noreply.github.com>
2025-05-10 18:49:38 -0500

    feat: se agrego clase cliente y relacion con clase reservation

--- src/main/java/agile/aresback/model/entity/Reservation.java
+++ src/main/java/agile/aresback/model/entity/Reservation.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
 @Data
 @Entity
 @Table(name = "Reservation")
@@ -29,27 +28,17 @@ public class Reservation {
     @Enumerated(EnumType.STRING)
     private StateReservation stateReservation;
 
-    // Datos del cliente directamente en la reserva
-    @Column(name = "nombre_cliente", nullable = false)
-    private String clienteNombre;
-
-    @Column(name = "apellido_cliente", nullable = false)
-    private String clienteApellido;
-
-    @Column(name = "email_cliente", nullable = false)
-    private String clienteEmail;
-
-    @Column(name = "telefono_cliente", nullable = false)
-    private String clienteTelefono;
-
-
-    //Relaciones
+    // Relaciones
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "table_id", nullable = false)
+    @JoinColumn(name = "mesa_id", nullable = false)
     private Mesa mesa;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id",nullable = false)
+    private Client client;
 }
+
